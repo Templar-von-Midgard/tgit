93c02776036c65b9bf565cf7a8c5c77540e7e680
Csonkás Kristóf Gyula <csonkas.kristof@gmail.com>
2019-02-12 21:02:59 +0100

    Diff scrolling synchronization via the scrollbars

--- app/tgit/src/FileDiffWidget.cpp
+++ app/tgit/src/FileDiffWidget.cpp
@@ -1,8 +1,11 @@
 #include "FileDiffWidget.hpp"
 
+#include <QDebug>
+
 #include <QtGui/QTextBlock>
 #include <QtGui/QTextBlockFormat>
 #include <QtGui/QTextCursor>
+#include <QtWidgets/QScrollBar>
 
 #include "ui_FileDiffWidget.h"
 
@@ -16,6 +19,38 @@ overload(Lambdas...)->overload<Lambdas...>;
 
 FileDiffWidget::FileDiffWidget(QWidget* parent) : QSplitter(parent), Ui(new Ui::FileDiffWidget) {
   Ui->setupUi(this);
+  connect(Ui->OldFileContents->verticalScrollBar(), &QScrollBar::valueChanged, this, [this](int value) {
+    if (ScrollLocked) {
+      return;
+    }
+    ScrollLocked = true;
+    int lineNumber = value + 1;
+    for (auto mapping : LineMapping) {
+      if (mapping.Left.contains(lineNumber)) {
+        int targetLineNumber = mapping.leftToRight(lineNumber);
+        auto targetScrollBar = Ui->NewFileContents->verticalScrollBar();
+        targetScrollBar->setValue(std::min(targetLineNumber - 1, targetScrollBar->maximum()));
+        break;
+      }
+    }
+    ScrollLocked = false;
+  });
+  connect(Ui->NewFileContents->verticalScrollBar(), &QScrollBar::valueChanged, this, [this](int value) {
+    if (ScrollLocked) {
+      return;
+    }
+    ScrollLocked = true;
+    int lineNumber = value + 1;
+    for (auto mapping : LineMapping) {
+      if (mapping.Right.contains(lineNumber)) {
+        int targetLineNumber = mapping.rightToLeft(lineNumber);
+        auto targetScrollBar = Ui->OldFileContents->verticalScrollBar();
+        targetScrollBar->setValue(std::min(targetLineNumber - 1, targetScrollBar->maximum()));
+        break;
+      }
+    }
+    ScrollLocked = false;
+  });
 }
 
 FileDiffWidget::~FileDiffWidget() = default;
@@ -28,6 +63,7 @@ void FileDiffWidget::setDiff(const DiffView* diff) {
 
 void FileDiffWidget::setFile(const DiffView::File* file) {
   CurrentFile = file;
+  LineMapping = {};
   refresh();
 }
 
@@ -44,22 +80,79 @@ void FileDiffWidget::refresh() {
 }
 
 void FileDiffWidget::highlightLines() {
-  auto visitor =
-      overload{[this](DiffView::AddedLine line) {
-                 QTextCursor newCursor{Ui->NewFileContents->document()->findBlockByLineNumber(line.LineNumber - 1)};
-                 auto newFormat = newCursor.blockFormat();
-                 QColor background{Qt::darkGreen};
-                 newFormat.setBackground(background.darker(150));
-                 newCursor.setBlockFormat(newFormat);
-               },
-               [this](DiffView::DeletedLine line) {
-                 QTextCursor oldCursor{Ui->OldFileContents->document()->findBlockByLineNumber(line.LineNumber - 1)};
-                 auto oldFormat = oldCursor.blockFormat();
-                 oldFormat.setBackground(QColor{Qt::darkRed}.darker(150));
-                 oldCursor.setBlockFormat(oldFormat);
-               },
-               [](DiffView::ContextLine) {}};
+  auto visitor = overload{
+      [this](DiffView::AddedLine line) {
+        if (LineMapping.empty()) {
+          LineMapping.push_back({Mapping::Addition, {0, 0}, {line.LineNumber, line.LineNumber}});
+        } else {
+          auto& [kind, left, right] = LineMapping.back();
+          if (kind == Mapping::Addition) {
+            right.extend(line.LineNumber);
+          } else {
+            LineMapping.push_back({Mapping::Addition, {left.Last, left.Last}, {line.LineNumber, line.LineNumber}});
+          }
+        }
+        QTextCursor newCursor{Ui->NewFileContents->document()->findBlockByLineNumber(line.LineNumber - 1)};
+        auto newFormat = newCursor.blockFormat();
+        QColor background{Qt::darkGreen};
+        newFormat.setBackground(background.darker(150));
+        newCursor.setBlockFormat(newFormat);
+      },
+      [this](DiffView::DeletedLine line) {
+        if (LineMapping.empty()) {
+          LineMapping.push_back({Mapping::Deletion, {line.LineNumber, line.LineNumber}, {0, 0}});
+        } else {
+          auto& [kind, left, right] = LineMapping.back();
+          if (kind == Mapping::Deletion) {
+            left.extend(line.LineNumber);
+          } else {
+            LineMapping.push_back({Mapping::Deletion, {line.LineNumber, line.LineNumber}, {right.Last, right.Last}});
+          }
+        }
+        QTextCursor oldCursor{Ui->OldFileContents->document()->findBlockByLineNumber(line.LineNumber - 1)};
+        auto oldFormat = oldCursor.blockFormat();
+        oldFormat.setBackground(QColor{Qt::darkRed}.darker(150));
+        oldCursor.setBlockFormat(oldFormat);
+      },
+      [this](DiffView::ContextLine line) {
+        if (LineMapping.empty()) {
+          LineMapping.push_back(
+              {Mapping::Context, {line.OldLineNumber, line.OldLineNumber}, {line.NewLineNumber, line.NewLineNumber}});
+          return;
+        }
+        auto& [kind, left, right] = LineMapping.back();
+        if (kind == Mapping::Context && left.Last + 1 == line.OldLineNumber && right.Last + 1 == line.NewLineNumber) {
+          left.extend(line.OldLineNumber);
+          right.extend(line.NewLineNumber);
+        } else {
+          LineMapping.push_back(
+              {Mapping::Context, {line.OldLineNumber, line.OldLineNumber}, {line.NewLineNumber, line.NewLineNumber}});
+        }
+      }};
   for (const auto& line : CurrentFile->Lines) {
     std::visit(visitor, line);
+  }
+  if (LineMapping.empty()) {
+    return;
+  }
+  auto& [firstKind, firstLeft, firstRight] = LineMapping.front();
+  if (firstKind == Mapping::Context) {
+    firstLeft.extend(1);
+    firstRight.extend(1);
+  }
+  auto& [lastKind, lastLeft, lastRight] = LineMapping.back();
+  if (lastKind == Mapping::Context) {
+    int oldLineCount = Ui->OldFileContents->document()->lineCount();
+    lastLeft.extend(oldLineCount);
+    int newLineCount = Ui->NewFileContents->document()->lineCount();
+    lastRight.extend(newLineCount);
+  }
+  for (auto it = LineMapping.begin(); it + 1 != LineMapping.end(); ++it) {
+    auto& previous = *it;
+    auto& next = *(it + 1);
+    if (previous.Kind == Mapping::Context && previous.Kind == next.Kind) {
+      previous.Left.extend(next.Left.First - 1);
+      previous.Right.extend(next.Right.First - 1);
+    }
   }
 }
--- app/tgit/src/FileDiffWidget.hpp
+++ app/tgit/src/FileDiffWidget.hpp
@@ -1,7 +1,10 @@
 #ifndef FILEDIFFWIDGET_HPP
 #define FILEDIFFWIDGET_HPP
 
+#include <cmath>
 #include <memory>
+#include <tuple>
+#include <vector>
 
 #include <QtWidgets/QSplitter>
 
@@ -10,6 +13,49 @@
 namespace Ui {
 class FileDiffWidget;
 }
+
+struct Region {
+  int First;
+  int Last;
+
+  constexpr bool contains(int value) const noexcept {
+    return First <= value && value <= Last;
+  }
+
+  constexpr void extend(int value) noexcept {
+    if (value < First) {
+      First = value;
+    }
+    if (Last < value) {
+      Last = value;
+    }
+  }
+
+  constexpr int offsetOf(int value) const noexcept {
+    return value - First;
+  }
+
+  constexpr int length() const noexcept {
+    return Last - First + 1;
+  }
+};
+
+struct Mapping {
+  enum Kind { Context, Addition, Deletion } Kind;
+  Region Left;
+  Region Right;
+
+  int leftToRight(int value) const noexcept {
+    double offset = Left.offsetOf(value);
+    auto ratio = offset / Left.length();
+    return std::round(Right.First + Right.length() * ratio);
+  }
+  int rightToLeft(int value) const noexcept {
+    double offset = Right.offsetOf(value);
+    auto ratio = offset / Right.length();
+    return std::round(Left.First + Left.length() * ratio);
+  }
+};
 
 class FileDiffWidget : public QSplitter {
   Q_OBJECT
@@ -28,6 +74,10 @@ private:
 
   const DiffView* CurrentDiff = nullptr;
   const DiffView::File* CurrentFile = nullptr;
+
+  std::vector<Mapping> LineMapping;
+
+  bool ScrollLocked = false;
 };
 
 #endif // FILEDIFFWIDGET_HPP
